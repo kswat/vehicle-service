a1648d4fd01972e322b0aff6780742334da52f25
sarav <sarav@demo.com>
2024-08-10 12:19:35 +0100

    Initial commit

---
+++ src/main/java/com/example/TraceConfig.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class TraceConfig {
+
+    // To have the @Observed support we need to register this aspect
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
